8168d6bc4ece4847f45c7644d413d12eea3acf84
Ronin Gomez <rgomez9@uncc.edu>
2020-10-26 17:51:06 -0400

    flask app changed url to index

--- flask_app/flask01.py
+++ flask_app/flask01.py
@@ -17,6 +17,7 @@ app = Flask(__name__)     # create an app
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/')
+@app.route('/index')
 def index():
     return 'Welcome, Notes App User!'
 
