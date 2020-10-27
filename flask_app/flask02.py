74f9f15e84468533f8c207cd21fa5e74211ba072
Ronin Gomez <rgomez9@uncc.edu>
2020-10-27 15:19:17 -0400

    flask app #2 - added a third note

--- flask_app/flask02.py
+++ flask_app/flask02.py
@@ -30,7 +30,8 @@ def get_user(username):
 @app.route('/notes')
 def get_notes():
     notes = {1 : {'title' : 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-                   2 : {'title' : 'Second note', 'text' : 'This is my second note', 'date' : '10-2-2020'}
+                   2: {'title' : 'Second note', 'text' : 'This is my second note', 'date' : '10-2-2020'},
+                   3: {'title' : 'Third note', 'text' : 'This is my third note', 'date': '10-3-2020'} 
                    }
     
     return render_template('notes.html', notes = notes)
@@ -38,7 +39,8 @@ def get_notes():
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     notes = {1 : {'title' : 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-                   2 : {'title' : 'Second note', 'text' : 'This is my second note', 'date' : '10-2-2020'}
+                   2 : {'title' : 'Second note', 'text' : 'This is my second note', 'date' : '10-2-2020'},
+                   3: {'title' : 'Third note', 'text' : 'This is my third note', 'date': '10-3-2020'}
                    }
     return render_template('note.html', note = notes[int(note_id)])
 
