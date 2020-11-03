6292919c4631fc76c30d6a33b28ed818e40cc286
Ronin Gomez <rgomez9@uncc.edu>
2020-11-03 12:43:00 -0500

    flask app #3 - added personalized header to all views

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -30,20 +30,24 @@ def get_user(username):
 
 @app.route('/notes')
 def get_notes():
+    a_user = {'name': 'Ronin', 'email':'mogli@uncc.edu'}
+
     notes = {1 : {'title' : 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
                    2: {'title' : 'Second note', 'text' : 'This is my second note', 'date' : '10-2-2020'},
                    3: {'title' : 'Third note', 'text' : 'This is my third note', 'date': '10-3-2020'} 
                    }
     
-    return render_template('notes.html', notes = notes)
+    return render_template('notes.html', notes = notes, user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = {'name': 'Ronin', 'email':'mogli@uncc.edu'}
+    
     notes = {1 : {'title' : 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
                    2 : {'title' : 'Second note', 'text' : 'This is my second note', 'date' : '10-2-2020'},
                    3: {'title' : 'Third note', 'text' : 'This is my third note', 'date': '10-3-2020'}
                    }
-    return render_template('note.html', note = notes[int(note_id)])
+    return render_template('note.html', note = notes[int(note_id)], user = a_user)
 
 @app.route('/notes/new')
 def new_note():
