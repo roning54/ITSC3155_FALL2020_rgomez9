db7c6dc2f0622004af4ca51bd2dd5536d6709a35
Ronin Gomez <rgomez9@uncc.edu>
2020-11-03 12:25:11 -0500

    flask app #3 - added new note route and view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -45,8 +45,13 @@ def get_note(note_id):
                    }
     return render_template('note.html', note = notes[int(note_id)])
 
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name': 'Ronin', 'email':'mogli@uncc.edu'}
+    return render_template('new.html', user = a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
