492f1d43efa0419cf568e4bbdd476d083089b6aa
Ronin Gomez <rgomez9@uncc.edu>
2020-12-03 13:19:08 -0500

    login registration functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -14,12 +14,16 @@ from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for 
+from flask import session
 from database import db
 from models import Note as Note
 from models import User as User
+from forms import RegisterForm
+from forms import LoginForm
 
 app = Flask(__name__)     # create an app
 
+app.config['SECRET_KEY'] = 'SE3155'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -52,11 +56,12 @@ def get_user(username):
 @app.route('/notes')
 def get_notes():
     #retrieve user from database
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
-    #retrieve notes from database
-    my_notes = db.session.query(Note).all()
-    
-    return render_template('notes.html', notes = my_notes, user = a_user)
+    if session.get('user'):
+        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
+
+        return render_template('notes.html', notes=my_notes, user=session['user'])
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -128,6 +133,26 @@ def delete_note(note_id):
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
+@app.route('/register', methods= ['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+
+        password_hash = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        new_record = User(first_name, last_name, request.form['email'], password_hash)
+        db.session.add(new_record)
+        db.session.commit()
+
+        session['user'] = first_name
+        the_user = db.session.query(User).filter_by(email = request.form['email']).one()
+        session['user_id'] = the_user.id
+        
+        return redirect(url_for('get_notes'))
+    return render_template('register.html', form=form)
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
